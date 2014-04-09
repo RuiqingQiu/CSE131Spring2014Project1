a47e29e652fdd6fabe71058d43971951b0dab339
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2014-04-09 11:25:51 -0700

    bitwise done

---
+++ src/BwXorOp.java
@@ -0,0 +1,18 @@
+
+public class BwXorOp extends BitwiseOp{
+	STO checkOperands(STO a, STO b) {
+		Type aType = a.getType();
+		Type bType = b.getType();
+		//Both operands must be bool type
+		if (aType instanceof IntType && bType instanceof IntType) {
+			return new ExprSTO("BwAndOp", aType);
+		}
+		else {
+			// error
+			if(!(aType instanceof IntType))
+				return new ErrorSTO(Formatter.toString(ErrorMsg.error1w_Expr,aType.getName(), "^", "int"));
+			else
+				return new ErrorSTO(Formatter.toString(ErrorMsg.error1w_Expr,bType.getName(), "^", "int"));
+		} 
+	}
+}
