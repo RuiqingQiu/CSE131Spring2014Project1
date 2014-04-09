bc5b280deedcb52e344940cd86ac7bb23d67c9f8
WM3 <margartewm3@gmail.com>
2014-04-08 23:26:35 -0700

    check#1 mul, div, mod added

---
+++ src/MulOp.java
@@ -0,0 +1,17 @@
+
+public class MulOp extends ArithmeticOp{
+    @Override
+	STO checkOperands(STO a, STO b) {
+		Type aType = a.getType();
+		Type bType = b.getType();
+		if (!(aType instanceof NumericType) || !(bType  instanceof NumericType)) {
+			// error
+			return new ErrorSTO(Formatter.toString(ErrorMsg.error1n_Expr,"bool", "*"));
+		} else if (aType instanceof IntType && bType instanceof IntType) {
+			//Calculate the value of two Ints
+			return new ExprSTO("MulOp", aType);
+		} else {
+			return new ExprSTO("MulOp", new FloatType("Float", 32));
+		}
+	}
+}
