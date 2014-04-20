4696f0b225957afed449aab0a6f0292242f85ec9
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2014-04-19 22:47:30 -0700

    check#17 test case done

---
+++ src/NullPointerType.java
@@ -0,0 +1,17 @@
+
+public class NullPointerType extends PointerType {
+
+	public NullPointerType(String strName, int size) {
+		super("nullptr", 4);
+	}
+	
+	public boolean isAssignableTo(Type t){
+		if(t.isPointer()){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
+
+}
