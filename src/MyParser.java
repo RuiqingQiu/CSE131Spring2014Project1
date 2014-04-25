2fe718c51f6fc5b0a5ad6989351f60d19dfb900b
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2014-04-25 13:12:43 -0700

    new delete funcptr fixed

--- src/MyParser.java
+++ src/MyParser.java
@@ -200,7 +200,11 @@ class MyParser extends parser
 			m_errors.print (ErrorMsg.error16_New_var);
 			return;
 		}
-		
+		if(sto.getType().isNullPointer()){
+			m_nNumErrors++;
+			m_errors.print(Formatter.toString(ErrorMsg.error16_New,sto.getType().getName()));
+			return;
+		}
 		//type of sto is not a valid pointer type
 		if(!(sto.getType().isGeneralPointer())){
 			m_nNumErrors++;
@@ -220,7 +224,11 @@ class MyParser extends parser
 			m_errors.print (ErrorMsg.error16_Delete_var);
 			return;
 		}
-		
+		if(sto.getType().isNullPointer()){
+			m_nNumErrors++;
+			m_errors.print(Formatter.toString(ErrorMsg.error16_New,sto.getType().getName()));
+			return;
+		}
 		//type of sto is not a valid pointer type
 		if(!(sto.getType().isGeneralPointer())){
 			m_nNumErrors++;
