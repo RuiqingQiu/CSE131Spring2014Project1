ad0033e66716f19ec6ad561e6059cc5402c92b43
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2014-04-25 19:15:08 -0700

    inc dec fixed

--- src/DecOp.java
+++ src/DecOp.java
@@ -12,6 +12,9 @@ public class DecOp extends UnaryOp{
 				return new ErrorSTO(Formatter.toString(ErrorMsg.error2_Lval,  "--"));		
 		}
 		else if(aType.isPointer()){
+			if(!(a.isModLValue())){
+				return new ErrorSTO(Formatter.toString(ErrorMsg.error2_Lval,  "--"));
+			}
 			//check if aType is a funcptr type
 			if(aType.isFuncPointer()){
 				return new ErrorSTO(Formatter.toString(ErrorMsg.error2_Type, aType.getName(), "--"));
--- src/IncOp.java
+++ src/IncOp.java
@@ -11,6 +11,9 @@ public class IncOp extends UnaryOp {
 				return new ErrorSTO(Formatter.toString(ErrorMsg.error2_Lval,  "++"));
 		} 
 		else if(aType.isPointer()){
+			if(!(a.isModLValue())){
+				return new ErrorSTO(Formatter.toString(ErrorMsg.error2_Lval,  "++"));
+			}
 			//check if aType is a functptr type
 			if(aType.isFuncPointer()){
 				return new ErrorSTO(Formatter.toString(ErrorMsg.error2_Type, aType.getName(), "++"));
